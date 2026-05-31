933ea606eb490236f4125a0760868c5ffefc411e
Vasil Tsvetkov <vasiltsvetkov20@gmail.com>
2026-05-31 17:44:33 +0300

    Implement and style identity forgot password, resend confirmation, and access denied pages

---
+++ ProjectManagementSystem.Web/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectManagementSystem.Web.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [AllowAnonymous]
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
